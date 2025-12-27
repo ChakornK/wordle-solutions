50e62cea433c104fd91d1872345e566c4611e4b7
ChakornK <chakornk2007@gmail.com>
2025-12-26 19:21:29 -0800

    feat: add 404 page

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,7 +1,11 @@
 export function NotFound() {
   return (
-    <main className="flex h-full flex-col items-center justify-center p-8">
-      <h1 className="text-6xl">404</h1>
+    <main class="text-text-light dark:text-text-dark h-dvh flex flex-col items-center justify-center p-8">
+      <h1 class="text-6xl font-bold">404</h1>
+      <p>Page not found</p>
+      <a href="/" class="mt-4 underline">
+        Return to home page
+      </a>
     </main>
   );
 }
