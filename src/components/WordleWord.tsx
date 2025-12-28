2d590a01e4307f88b926524363585c8bcac3b982
ChakornK <chakornk2007@gmail.com>
2025-12-28 12:28:46 -0800

    feat: add letter reveal animation

--- src/components/WordleWord.tsx
+++ src/components/WordleWord.tsx
@@ -1,28 +1,71 @@
+import { useEffect, useState } from "preact/hooks";
 import { LetterStatus, type WordleGuess } from "../types/wordle";
+import { stagger, useAnimate } from "motion/react";
 
 const variants = [
   "text-text-dark bg-wordle-absent-light dark:bg-wordle-absent-dark",
   "text-text-dark bg-wordle-present-light dark:bg-wordle-present-dark",
   "text-text-dark bg-wordle-correct-light dark:bg-wordle-correct-dark",
-  "border border-2 border-border-light dark:border-border-dark",
-  "border border-2 border-border-faint-light dark:border-border-faint-dark",
+  "border border-2 bg-bg-light dark:bg-bg-dark border-border-light dark:border-border-dark",
+  "border border-2 bg-bg-light dark:bg-bg-dark border-border-faint-light dark:border-border-faint-dark",
 ];
 
 export const WordleWord = ({ guess }: { guess: WordleGuess }) => {
+  const [scope, animate] = useAnimate();
+  const [pending, setPending] = useState<boolean>(
+    guess.every((l) => l.status === LetterStatus.pending)
+  );
+  useEffect(() => {
+    if (!pending) {
+      animate(
+        ".letter-front",
+        {
+          scaleY: [1, 0, 0, 1],
+          opacity: [1, 1, 0, 0],
+        },
+        {
+          delay: stagger(0.25),
+          duration: 0.5,
+          times: [0, 0.5, 0.5, 1],
+        }
+      );
+      animate(
+        ".letter-back",
+        {
+          scaleY: [1, 0, 1],
+        },
+        {
+          delay: stagger(0.25),
+          duration: 0.5,
+        }
+      );
+    }
+  }, [pending]);
+
   return (
-    <div class="flex gap-[0.25em]">
+    <div class="flex gap-[0.25em]" ref={scope}>
       {Array.from({ length: 5 })
         .map((_, i) => guess[i] || { letter: "", status: LetterStatus.pending })
         .map((letter, i) => (
-          <div
-            key={i}
-            class={`flex h-[2em] w-[2em] items-center justify-center font-bold uppercase ${
-              variants[
-                letter.letter == "" ? LetterStatus.pending + 1 : letter.status
-              ]
-            }`}
-          >
-            {letter.letter}
+          <div key={i} class="relative h-[2em] w-[2em] font-bold uppercase">
+            <div
+              class={`letter-back absolute inset-0 flex items-center justify-center ${
+                variants[letter.status]
+              }`}
+            >
+              {letter.letter}
+            </div>
+            <div
+              class={`letter-front absolute inset-0 flex items-center justify-center ${
+                variants[
+                  letter.letter == ""
+                    ? LetterStatus.pending + 1
+                    : LetterStatus.pending
+                ]
+              }`}
+            >
+              {letter.letter}
+            </div>
           </div>
         ))}
     </div>
